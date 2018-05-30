83c23ad6f1e0173b47d3a0b1d5e17fde1088f492
André Victor <andrevctr12@gmail.com>
2018-05-30 19:21:38 -0300

    Mapping root /

--- src/main/java/br/com/unioeste/oficinamecanicapg/Application.java
+++ src/main/java/br/com/unioeste/oficinamecanicapg/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class Application {
@@ -11,24 +13,11 @@ public class Application {
    public static void main(String[] args) {
       SpringApplication.run(Application.class, args);
    }
+
+   @GetMapping("/")
+   @ResponseBody
+   public String home()
+   {
+      return "home";
+   }
 }
-//
-//@Configuration
-//public class MainConfig {
-//
-//   @Bean
-//   public BasicDataSource dataSource() throws URISyntaxException {
-//      URI dbUri = new URI(System.getenv("DATABASE_URL"));
-//
-//      String username = dbUri.getUserInfo().split(":")[0];
-//      String password = dbUri.getUserInfo().split(":")[1];
-//      String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
-//
-//      BasicDataSource basicDataSource = new BasicDataSource();
-//      basicDataSource.setUrl(dbUrl);
-//      basicDataSource.setUsername(username);
-//      basicDataSource.setPassword(password);
-//
-//      return basicDataSource;
-//   }
-//}
